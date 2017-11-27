5f00b9dbb4fbdb830b3d22034436c4a3c863c408
Daniel Moreto <dfmoreto@gmail.com>
2017-11-27 18:15:42 -0200

    Create default policy and change Movie migration

--- db/migrate/20171127151149_create_rentals.rb
+++ db/migrate/20171127151149_create_rentals.rb
@@ -3,6 +3,7 @@ class CreateRentals < ActiveRecord::Migration[5.1]
     create_table :rentals do |t|
       t.datetime :date
       t.references :movie, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
